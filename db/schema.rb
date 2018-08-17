ee043ceaebc1c7e32a189e3c02ef51ae24781e19
tito Domingos <muandatito@gmail.com>
2018-08-17 03:16:02 +0200

    run the migration for table ingredients

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_16_103945) do
+ActiveRecord::Schema.define(version: 2018_08_17_011336) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "ingredients", force: :cascade do |t|
+  end
 
   create_table "recipes", force: :cascade do |t|
     t.string "name"
