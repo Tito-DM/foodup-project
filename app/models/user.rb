7c32c4ea1831acca59804515f0e36ce4da77397c
tito Domingos <muandatito@gmail.com>
2018-08-28 00:59:06 +0200

    change the name of attr password to password_digest, create a session controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ApplicationRecord
   has_many :recipes
+  before_save { self.email = email.downcase}
+  validates :name, presence:true, length: {minimum:3, maximum:50}
+  VALID_EMAIL_REGX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence:true, uniqueness: {case_sensitive: false},format: {with:VALID_EMAIL_REGX}
+
+  has_secure_password
 end
