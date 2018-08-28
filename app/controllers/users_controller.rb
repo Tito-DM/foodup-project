d92f981dd4166a2ef9ccd4582d0a8f441e36c555
tito Domingos <muandatito@gmail.com>
2018-08-28 13:49:23 +0200

    Add sign in upon sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
     before_action :set_user, only: [:edit, :update, :show]
-    before_action :require_user
+    before_action :require_user, except: [:new,:create]
     before_action :require_same_user, only: [:edit,:update,:show,:destroy]
 
   def new
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id #allow to login when registered
       flash[:notice] = 'Profile was successfully created'
       redirect_to user_path(@user)
     else
