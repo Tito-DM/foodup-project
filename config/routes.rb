e12a17c98a56c72cb72898a6f6c1fab02dd423df
tito Domingos <muandatito@gmail.com>
2018-09-02 12:51:07 +0200

    fix search problem without login

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'index', to: 'recipes#index'
+  root 'recipes#index'
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
-  root 'recipes#index'
+
   resources :users do
-    resources :recipes  do
+    resources :recipes,except: :index  do
       get :view, on: :member
       resources :ingredients
       resources :steps
