fed8bb68e3e309719d1a4dfd50508fc0fce55e32
tito Domingos <muandatito@gmail.com>
2018-09-03 21:19:58 +0200

    fixe file migration changecolumnnamepassword

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_03_173949) do
+ActiveRecord::Schema.define(version: 2018_09_02_204935) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,20 @@ ActiveRecord::Schema.define(version: 2018_09_03_173949) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "recipe_id"
+  end
+
+  create_table "lovelikes", force: :cascade do |t|
+    t.integer "dislike"
+    t.integer "love_it"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "rates", force: :cascade do |t|
+    t.integer "rate_number"
+    t.integer "recipe_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "recipes", force: :cascade do |t|
